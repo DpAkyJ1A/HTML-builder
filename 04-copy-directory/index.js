5aa426cb846048951bbd51a53448708865b2a80b
Aleksey Zvagolsky <alekseyzvagolsky@gmail.com>
2022-05-19 16:18:16 +0300

    add: implement 04-copy-directory (Thu, May 19, 2022 04:18PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require("fs");
+const path = require("path");
+const dir = path.resolve(__dirname, "files");
+const dirCopy = path.resolve(__dirname, "files-copy");
 
+function createFolder() {
+  fs.mkdir(dirCopy, (err) => {
+    if (err) throw err;
+  });
+}
+
+function copyDir() {
+  fs.readdir(dirCopy, (err) => {
+    if (err) createFolder();
+    fs.readdir(dir, (err, files) => {
+      if (err) throw err;
+      files.forEach((file) => {
+        fs.createReadStream(path.resolve(dir, file)).pipe(
+          fs.createWriteStream(path.resolve(dirCopy, file))
+        );
+      });
+    });
+  });
+}
+
+copyDir();
