e872fa975e179204acc3832cc47b0fa03e7dd012
Aleksey Zvagolsky <alekseyzvagolsky@gmail.com>
2022-05-19 14:23:14 +0300

    add: implement 01-read-file and 02-write-file (Thu, May 19, 2022 02:23PM)

---
+++ 02-write-file/index.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+const { stdin, stdout } = process;
+
+stdout.write("Write something: ");
+stdin.on("data", (data) => {
+  if (data.toString().substring(0, data.length - 2) == "exit") process.exit();
+  fs.access("file.txt", fs.constants.F_OK, (err) => {
+    if (err) init();
+    fs.appendFile(path.join(__dirname, "file.txt"), data, (err) => {
+      if (err) throw err;
+    });
+    stdout.write("Write something: ");
+  });
+});
+
+process.on("exit", () => stdout.write("What did u write in me..."));
+
+process.on("SIGINT", () => process.exit());
+
+function init() {
+  fs.writeFile("file.txt", "", (error) => {
+    if (error) return console.error(error.message);
+  });
+}
